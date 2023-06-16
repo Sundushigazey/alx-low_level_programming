012eb18682976369026c56a55c8de511068d3b9d
sundus <sundus.w.fairouz@gmail.com>
2023-06-15 17:05:43 -0700

    error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints the size of various types on the computer it is compiled and run on.
+ * Description: A C program that prints the size of various types of data type.
  *
  * Return: Always 0 (Success)
 */
