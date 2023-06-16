e2974d6b2a61d65f6e737cc0be797cb12c29818f
sundus <sundus.w.fairouz@gmail.com>
2023-06-15 17:35:50 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@ int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int s = 0;
-	while(quo[s] != '\0')
+
+	while (quo[s] != '\0')
 	{
 		putchar(quo[s]);
 		s++;
