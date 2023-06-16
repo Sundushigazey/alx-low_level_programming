927b29d3d463610f527f4f6c1f2853975b118b6b
sundus <sundus.w.fairouz@gmail.com>
2023-06-15 17:19:30 -0700

    error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include<unistd.h>
-int main (void) {
+/**
+ * main - Entry point
+ *
+ * Return: return 1
+ */
+int main(void)
+{
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int s = 0;
-	while (quote[s] != '\0') {
+	while (quote[s] != '\0')
+	{
 		putchar(quote[s]);
 		s++
 	}
