a97a182fd41275336e483a4785f3a23308b1833a
sundus <sundus.w.fairouz@gmail.com>
2023-06-15 17:02:45 -0700

    error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
