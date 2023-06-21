d8b7eea895280e1a4a3da3d8524996194cd98eb8
sundus <sundus.w.fairouz@gmail.com>
2023-06-21 16:55:33 -0700

    d

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,6 @@ if (month >= 2 && day >= 60)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -33,6 +32,5 @@ else
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
-
 }
 }
