288317d40e0fba9ec7af14f36d088925970fa654
sundus <sundus.w.fairouz@gmail.com>
2023-06-20 18:06:09 -0700

    error

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
 * times_table - prints the 9 times table, starting with 0
+* example table
+* 0, 0, 0, 
+* 0, 1, 2,
 */
 void times_table(void)
 {
@@ -13,8 +16,9 @@ for (mul = 0; mul <= 9; mul++)
 _putchar(',');
 _putchar(' ');
 prod = num * mul;
-if (prod > 9)
-{
+if (prod <= 9)
+_putchar(' ');
+else
 _putchar((prod / 10) + 48);
 _putchar((prod % 10) + 48);
 }
