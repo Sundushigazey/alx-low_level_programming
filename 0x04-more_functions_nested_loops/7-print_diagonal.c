7c8c0997724fa23dc0a9b308951054a5420a5473
sundus <sundus.w.fairouz@gmail.com>
2023-06-22 15:28:34 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,11 +11,20 @@ _putchar('\n');
 }
 else
 {
-int i;
-for (i = 1; i <= n; i++)
+int i, j;
+for (i = 0; i <= n; i++)
 {
-_putchar('\');
+for (j = 0; j <= n; j++)
+{
+if (j == i)
+{
+_putchar('\\');
 }
+else if (j < i)
+{
+_putchar(' ');
 }
 _putchar('\n');
+}
+}
 }
