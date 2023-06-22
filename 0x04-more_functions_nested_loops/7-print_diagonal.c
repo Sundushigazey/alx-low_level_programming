602e4d86101001a2cec0000076b3ee0b3d23921d
sundus <sundus.w.fairouz@gmail.com>
2023-06-22 15:50:05 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,25 +6,15 @@
 void print_diagonal(int n)
 {
 int i, j;
-if (n <= 0)
-{
+if  (n <= 0)
 _putchar('\n');
-}
 else
 {
-for (i = 0; i <= n; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0; j <= n; j++)
-{
-if (j == i)
-{
-_putchar('\');
-}
-else if (j < i)
-{
+for (j = 0; j < n; j++)
 _putchar(' ');
-}
-}
+_putchar(92);
 _putchar('\n');
 }
 }
