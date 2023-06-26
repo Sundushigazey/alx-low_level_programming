e700b57b82e018a3a6f5962dc89c6e1495568356
sundus <sundus.w.fairouz@gmail.com>
2023-06-26 16:38:32 -0700

    task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - function that reverses a string.
+ * @s : pointer to string to be reversed
+ */
+void rev_string(char *s)
+{
+char tmp;
+int i, l;
+for (l = 0; s[l] != '\0'; l++)
+{
+;
+}
+for (i = 0; i < l / 2; i++)
+{
+tmp = s[i];
+s[i] = s[l - 1 - i];
+s[l - 1 - i] = tmp;
+}
+}
