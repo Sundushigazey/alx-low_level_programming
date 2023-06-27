e4ff460af4587d740ce87e9a2150815cdeda31ef
sundus <sundus.w.fairouz@gmail.com>
2023-06-26 17:11:44 -0700

    task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+£include "main.h"
+/**
+ * print_array -  prints n elements of an array followed by a new line.
+ * @a : pointer to array
+ * @n : nomber of elements in array
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == 0)
+{
+printf("%d", a[i]);
+}
+else
+{
+printf(", %d", a[i]);
+}
+}
+_putchar('\n');
+}
