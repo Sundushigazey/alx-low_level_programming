8cbc700ed93491bfa62a3241c97e09a7c7508fff
sundus <sundus.w.fairouz@gmail.com>
2023-06-26 17:23:44 -0700

    task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,9 +2,9 @@
 /**
  * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
- * @dest : pointer to the buffer in which we copy the string
- * @src : string to be copied
- * Return : the pointer to dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: (dest)
  */
 char *_strcpy(char *dest, char *src)
 {
