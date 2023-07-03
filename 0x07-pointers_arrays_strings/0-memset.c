0ae8faa9834c53d182411f313a014db4018d210e
sundus <sundus.w.fairouz@gmail.com>
2023-07-03 16:03:49 -0700

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: area of memory to be filled
+ * @b: constant byte
+ * @n: number of time to be filled
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+	{
+	s{i} = b;
+	}
+return (s);
+}
