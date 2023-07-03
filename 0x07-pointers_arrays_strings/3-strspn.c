cb96664edc9838c21228d79546811d555359e939
sundus <sundus.w.fairouz@gmail.com>
2023-07-03 16:33:30 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,33 @@
 #include "main.h"
-
 /**
- * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- *
- * Return: the number of bytes in the initial segment
- * of s which consist only of bytes from accept
- */
+* *_strspn - gets the length of a prefix substring
+* @s: string to evaluate
+* @accept: string containing the list of characters to match in s
+* Return: the number of bytes in the initial segment
+* of s which consist only of bytes from accept
+*/
 unsigned int _strspn(char *s, char *accept)
+{
+int i;
+int j;
+int f;
+int ff;
+f = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+ff = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+f++;
+ff = 1;
+}
+}
+if (ff == 0)
+{
+return (f);
+}
+}
+return (0);
+}
