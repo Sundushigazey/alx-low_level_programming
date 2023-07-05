f29212a46e8d41b57b49db752442f0deffc6905f
sundus <sundus.w.fairouz@gmail.com>
2023-07-05 15:43:35 -0700

    task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial- return factorial of a given number
+ * @n: number to get its factorial
+ * Return: factorial
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+factorial(n * factorial(n - 1);
+}
