09c060a0b5005f4bf214d17fc414c109348cd2a9
sundus <sundus.w.fairouz@gmail.com>
2023-07-11 15:39:26 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+char *dup;
+unsigned int i, j;
+i = 0;
+j = 0;
+if (str == NULL)
+return (NULL);
+while (str[j])
+j++;
+dup = (char *) malloc(sizeof(char) * (j + 1));
+if (dup == NULL)
+return (NULL);
+while ((dup[i] = str[i]) != '\0')
+i++;
+return (dup);
+}
