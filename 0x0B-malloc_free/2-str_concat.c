8987f101ce8372ef34eab44fdb9b9535dbdbab20
sundus <sundus.w.fairouz@gmail.com>
2023-07-11 17:09:50 -0700

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * *str_concat - concatenates two strings
  * @s1: string to concatenate
@@ -9,15 +8,37 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *result;
-int l1 = strlen(s1);
-int l2 = strlen(s2);
-result = (char *) malloc((l1 + l2 + 1) * sizeof(char));
-if (result == NULL)
-{
+char *s3;
+unsigned int a = 0;
+unsigned int b = 0;
+unsigned int ln1 = 0;
+unsigned int ln2 = 0;
+while (s1 && s1[ln1])
+ln1++;
+while (s2 && s2[ln2])
+ln2++;
+s3 = malloc(sizeof(char) * (ln1 + ln2 + 1));
+if (s3 == NULL)
 return (NULL);
+a = 0;
+b = 0;
+if (s1)
+{
+while (a < ln1)
+{
+s3[a] = s1[a];
+a++;
 }
-strcpy(result, s1);
-strcat(result, s2);
-return (result);
+}
+if (s2)
+{
+while (a)
+{
+s3[a] = s2[b];
+a++;
+b++;
+}
+}
+s3[a] = '\0';
+return (s3);
 }
