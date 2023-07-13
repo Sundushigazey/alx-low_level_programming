26ae7b3993a465fce04d22c04702bf6dbe13df78
sundus <sundus.w.fairouz@gmail.com>
2023-07-13 15:48:16 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,24 +15,38 @@ int j = 0;
 int ln1 = 0;
 int ln2 = 0;
 while (s1 && s1[ln1])
+{
 ln1++;
+}
 while (s2 && s2[ln2])
+{
 ln2++;
+}
 if (n < ln2)
+{
 s3 = malloc(sizeof(char) * (ln1 + n + 1));
+}
 else
+{
 s3 = malloc(sizeof(char) * (ln1 + ln2 + 1));
+}
 if (!s3)
+{
 return (NULL);
+}
 while (i < ln1)
 {
 s3[i] = s1[i];
 i++;
 }
 while (n < ln2 && i < (ln1 + n))
+{
 s3[i++] = s2[j++];
+}
 while (n >= ln2 && i < (ln1 + ln2))
+{
 s3[i++] = s2[j++];
+}
 s3[i] = '\0';
 return (s3);
 }
