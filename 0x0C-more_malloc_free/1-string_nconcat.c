c0e7f9f2abf50514f094e8fbfbfa848a43ec8f00
sundus <sundus.w.fairouz@gmail.com>
2023-07-13 15:55:39 -0700

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
+#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
+
 /**
  * *string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
@@ -10,30 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s3;
-unsigned int i = 0;
-int j = 0;
-int ln1 = 0;
-int ln2 = 0;
-while (s1 && s1[ln1])
-ln1++;
-while (s2 && s2[ln2])
-ln2++;
-if (n < ln2)
-s3 = (char *) malloc(sizeof(char) * (ln1 + n + 1));
-else
-s3 = (char *) malloc(sizeof(char) * (ln1 + ln2 + 1));
-if (!s3)
-return (NULL);
-while (i < ln1)
-{
-s3[i] = s1[i];
-i++;
-}
-while (n < ln2 && i < (ln1 + n))
-s3[i++] = s2[j++];
-while (n >= ln2 && i < (ln1 + ln2))
-s3[i++] = s2[j++];
-s3[i] = '\0';
-return (s3);
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
