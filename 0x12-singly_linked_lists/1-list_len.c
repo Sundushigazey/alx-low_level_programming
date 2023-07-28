46ce0f2257b9cd8e5c3569b789997f8dbdd31ba3
sundus <sandymoon2022@yahoo.com>
2023-07-28 16:22:29 -0700

    task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len- returns the number of elements in a linked list
+ * @h : pointer tyo the lise
+ *
+ * Return : number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: pointer to the  list
+ * @str: new string to add
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+
+	n->str = strdup(str);
+	n->len = len;
+	n->next = (*head);
+	(*head) = n;
+
+	return (*head);
+}
