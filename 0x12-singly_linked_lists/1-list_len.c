f6f27d87c3eed4db17af37f3ce9a689efb642585
sundus <sandymoon2022@yahoo.com>
2023-07-28 16:30:17 -0700

    task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * list_len- returns the number of elements in a linked list
+ * list_len - returns the number of elements in a linked list
  * @h : pointer tyo the lise
  *
- * Return : number of elements in list
+ * Return: number of elements in list
  */
 size_t list_len(const list_t *h)
 {
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,20 +1,20 @@
-#include <string.h>
-#include <stdlib.h>
 #ifndef LISTS_H
 #define LISTS_H
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * struct lst1- singly linked list
- * @str: string
- * @len: length
- * @next: next node
- */
+* struct list_s- singly linked list
+* @str: string
+* @len: length
+* @next: next node
+*/
 typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct st *next;
-}list_t;
+char *str;
+unsigned int len;
+struct st *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
