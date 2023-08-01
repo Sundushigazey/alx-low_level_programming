84ab7bd535d8938ee2c43902c668d3fe1c8130a2
sundus <sandymoon2022@yahoo.com>
2023-08-01 15:27:39 -0700

    task

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free linked list
+ * @head: listint_t list to free
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tp;
+
+	while (head)
+	{
+		tp = head->next;
+		free(head);
+		head = tp;
+	}
+}
